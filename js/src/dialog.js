127e8dd2ce490e9a929a9473a9315ef46065511b
Neep <343283676@qq.com>
2019-09-16 15:22:00 +0800

    change up_file

--- js/src/dialog.js
+++ js/src/dialog.js
@@ -1,37 +1,39 @@
-function show_dialog(option,callback) {
-    console.log(option)
-    $('#dialog').css('display','flex');
 
+var callback;
+function show_dialog(option,call) {
+
+    if (call){
+        callback=call;
+    }
+    $('#dialog').css('display','flex');
     if(option.cancel_show){
-        $('.dialog_cancel').css('display','none');
+        $('.dialog_cancel').hide();
     }
     $('.dialog_info').text(option.info);
 
     $('.dialog_define').text(option.define);
 
-    $('.dialog_define').on("tap",function (e) {
-
-       if($(e.target).hasClass('dialog_define')){
-
-           $('#dialog').css("display","none");
-           if (callback){
-               callback(true)
-           }
-       }
-    })
     $('.dialog_cancel').text(option.cancel);
-    $('.dialog_cancel').tap(function (e) {
-
-        if($(e.target).hasClass('dialog_cancel')){
-            $('#dialog').css("display","none");
-            if (callback){
-                callback(false)
-            }
-        }
-
-    })
-    // $('.dialog').click(function () {
-    //     $('#dialog').css("display","none");
-    // })
 
 };
+
+$('.dialog_define').click(function (e) {
+
+    console.log(callback)
+    if($(e.target).hasClass('dialog_define')){
+
+        $('#dialog').hide();
+
+        if (callback){
+            callback()
+        }
+    }
+});
+
+$('.dialog_cancel').on("tap",function (e) {
+
+    if($(e.target).hasClass('dialog_cancel')){
+        $('#dialog').hide();
+
+    }
+})
